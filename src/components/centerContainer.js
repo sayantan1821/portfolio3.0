6499a8b14e59a52b7853f7069210ff8ee78e36be
sayantan-k-hashcodejr <86970907+sayantan-k-hashcodejr@users.noreply.github.com>
2021-10-04 23:54:40 +0530

    update center content

--- src/components/centerContainer.js
+++ src/components/centerContainer.js
@@ -84,7 +84,36 @@ ReactDOM.render(
 );
 render() {
     return (
-        <div>
+        <div><div className="MarkdownEditor">
+        <h3>Input</h3>
+        <label htmlFor="markdown-content">import React from 'react'
+            import ReactDOM from 'react-dom'
+
+            import {Provider} from 'react-redux'
+            import store from './store'
+
+            import App from './App'
+
+            const rootElement = document.getElementById('root')
+            ReactDOM.render(
+            <Provider store={store}>
+                <App />
+            </Provider>,
+            rootElement
+            )
+            Enter some markdown
+        </label>
+        <textarea
+            id="markdown-content"
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+        />
+        <h3>Output</h3>
+        <div
+            className="content"
+            dangerouslySetInnerHTML={this.getRawMarkup()}
+        />
+    </div>
             <h1>{this.props.name}</h1>
             <p>
                 {this.props.color}
--- src/pages/secondPage.js
+++ src/pages/secondPage.js
@@ -112,6 +112,36 @@ class MarkdownEditor extends React.Component {
                     dangerouslySetInnerHTML={this.getRawMarkup()}
                 />
             </div>
+            <div className="MarkdownEditor">
+            <h3>Input</h3>
+            <label htmlFor="markdown-content">import React from 'react'
+                import ReactDOM from 'react-dom'
+
+                import {Provider} from 'react-redux'
+                import store from './store'
+
+                import App from './App'
+
+                const rootElement = document.getElementById('root')
+                ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>,
+                rootElement
+                )
+                Enter some markdown
+            </label>
+            <textarea
+                id="markdown-content"
+                onChange={this.handleChange}
+                defaultValue={this.state.value}
+            />
+            <h3>Output</h3>
+            <div
+                className="content"
+                dangerouslySetInnerHTML={this.getRawMarkup()}
+            />
+        </div>
         );
     }
 }
